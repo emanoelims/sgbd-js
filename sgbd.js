45dd745aaa1b71025be5a75a028cf52622e389fc
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-01-17 10:14:03 -0300

    solução para o segundo desafio
    com base no nome da tabela e nas colunas, monte uma estrutura de objetos para armazenar tanto a definição da tabela quanto os dados.
    
    utilizei nesse execício:
     - Object
     - for/of
     - JSON.stringify

--- sgbd.js
+++ sgbd.js
@@ -1,9 +1,22 @@
 const query = 'create table author (id number, name string, age number, city string, state string, country string)';
 const regexp = /^create table ([a-z]+)\s*\((.+)\)$/;
 const parsedQuery = query.match(regexp);
-
 const tableName = parsedQuery[1];
-const columns = parsedQuery[2].split(/,\s*/);
+const columnsQuery = parsedQuery[2].split(/,\s*/);
 
-console.log(tableName);
-console.log(columns);
+const database = {
+  tables: {
+    [tableName]: {
+      columns: {}
+    }
+  }
+};
+
+for (let columnQuery of columnsQuery) {
+  const column = columnQuery.split(' ');
+  const name = column[0];
+  const type = column[1];
+  database.tables[tableName].columns[name] = type;
+}
+
+console.log(JSON.stringify(database, undefined, '  '));
