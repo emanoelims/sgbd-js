8ba13ef6fe776433a5eb6d8cdbb9355b6bb0e84f
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-01-17 11:29:56 -0300

    solução do quinto desafio
    O desafio era implementar o método "insert". Para isso, era necessário, como sempre, extrair as informações a partir do comando, converter as informações em um objeto e inserir no array "data" da tabela correspondente.
    
    utilizei nesse desafio:
      - if
      - for
      - this
      - Method Notation
      - Array.prototype.push
      - String.prototype.match
      - String.prototype.split
      - String.prototype.startsWith

--- sgbd.js
+++ sgbd.js
@@ -11,6 +11,11 @@ const database = {
       return;
     }
 
+    if (statment.startsWith('insert into')) {
+      this.insert(statment);
+      return;
+    }
+
     const message = `Syntax error: '${statment}'`;
     throw new DatabaseError(statment, message);
   },
@@ -31,13 +36,29 @@ const database = {
       const type = column[1];
       this.tables[tableName].columns[name] = type;
     }
+  },
+  insert(query) {
+    const regexp = /^insert into ([a-z]+)\s*\((.+)\) values\s*\((.+)\)$/;
+    const parsedQuery = query.match(regexp);
+    const tableName = parsedQuery[1];
+    const columns = parsedQuery[2].split(/,\s*/);
+    const values = parsedQuery[3].split(/,\s*/);
+
+    const row = {};
+
+    columns.forEach((column, index) => {
+      row[column] = values[index];
+    })
+
+    this.tables[tableName].data.push(row);
   }
 };
 
-const query = 'create table author (id number, name string, age number, city string, state string, country string)';
-
 try {
-  database.execute(query);
+  database.execute('create table author (id number, name string, age number, city string, state string, country string)');
+  database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+  database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+  database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
 } catch (err) {
   console.log(err.message);
 }
