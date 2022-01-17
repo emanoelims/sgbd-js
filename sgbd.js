c22a095d2c7f9d6a409a3048971dfa65c652ce94
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-01-17 10:26:56 -0300

    solução para o terceiro desafio
    Criar dois métodos no objeto "database" chamados de "createTable" e "execute". O comando "createTable" já foi implementado no exercício anterior, mova o código e utilize o método "execute" para invocar dinamicamente o método "createTable".
    
    utilizei nesse exercício:
     - if
     - this
     - Method Notation
     - String.prototype.startsWith

--- sgbd.js
+++ sgbd.js
@@ -1,22 +1,30 @@
-const query = 'create table author (id number, name string, age number, city string, state string, country string)';
-const regexp = /^create table ([a-z]+)\s*\((.+)\)$/;
-const parsedQuery = query.match(regexp);
-const tableName = parsedQuery[1];
-const columnsQuery = parsedQuery[2].split(/,\s*/);
-
 const database = {
-  tables: {
-    [tableName]: {
-      columns: {}
+  tables: {},
+  execute(statment) {
+    if (statment.startsWith('create table')) {
+      this.createTable(statment);
+    }
+  },
+  createTable(query) {
+    const regexp = /^create table ([a-z]+)\s*\((.+)\)$/;
+    const parsedQuery = query.match(regexp);
+    const tableName = parsedQuery[1];
+    const columnsQuery = parsedQuery[2].split(/,\s*/);
+
+    this.tables[tableName] = {
+      columns: {},
+      data: []
+    }
+
+    for (let columnQuery of columnsQuery) {
+      const column = columnQuery.split(' ');
+      const name = column[0];
+      const type = column[1];
+      this.tables[tableName].columns[name] = type;
     }
   }
 };
 
-for (let columnQuery of columnsQuery) {
-  const column = columnQuery.split(' ');
-  const name = column[0];
-  const type = column[1];
-  database.tables[tableName].columns[name] = type;
-}
-
+const query = 'create table author (id number, name string, age number, city string, state string, country string)';
+database.execute(query);
 console.log(JSON.stringify(database, undefined, '  '));
